d9c5970b88931b6538cb9040853914bc05c4539c
Paul Campbell <PaCampbell@mgmresorts.com>
2024-06-13 02:47:03 -0400

    add google login

--- src/utils/context/useAuth.tsx
+++ src/utils/context/useAuth.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useStorageState } from "../hooks";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
 const AuthContext = React.createContext<{
   signIn: (data?: any) => void;
   signOut: () => void;
   session?: string | null;
   isLoading: boolean;
+  loginWithGoogle?: () => void;
+  error: any | null;
 }>({
   signIn: () => null,
   signOut: () => null,
   session: null,
   isLoading: false,
+  loginWithGoogle: () => null,
+  error: null,
 });
 
 // This hook can be used to access the user info.
@@ -27,10 +32,34 @@ export function useSession() {
 
 export function SessionProvider(props: React.PropsWithChildren) {
   const [[isLoading, session], setSession] = useStorageState("session");
+  const [error, setError] = useState<any | null>(null);
+  console.log("errpr", JSON.stringify(error));
+  const configureGoogleSignIn = () => {
+    GoogleSignin.configure({
+      webClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
+      iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
+      // androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
+    });
+  };
 
+  const loginWithGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      setSession(JSON.stringify(userInfo));
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    configureGoogleSignIn();
+  }, []);
   return (
     <AuthContext.Provider
       value={{
+        loginWithGoogle,
+        error,
         signIn: (data: any) => {
           // Perform sign-in logic here
           setSession(data || "xxx");
