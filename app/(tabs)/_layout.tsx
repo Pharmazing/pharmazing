748ebe971f559e7a68d94b8fb4cd2964c998b044
Paul Campbell <PaCampbell@mgmresorts.com>
2024-06-22 00:43:47 -0400

    add icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,13 +3,20 @@ import { Tabs } from "expo-router";
 import { Drawer } from "expo-router/drawer";
 import { isAndroid, isIOS, isWeb } from "../../src/utils";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import HomeIcon from "../../src/icons/HomeIcon";
 
 export default function Layout() {
   return (
     <>
       {(isIOS || isAndroid) && (
         <Tabs>
-          <Tabs.Screen name="home" />
+          <Tabs.Screen
+            name="home"
+            options={{
+              title: "Home",
+              tabBarIcon: () => <HomeIcon />,
+            }}
+          />
           <Tabs.Screen name="activity" />
           <Tabs.Screen name="cart" />
           <Tabs.Screen name="settings" />
