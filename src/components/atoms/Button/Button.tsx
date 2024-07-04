5041bb74c04dd8a9d0bc0611ef40650fcefb9b3a
Paul Campbell <PaCampbell@mgmresorts.com>
2024-07-03 22:52:17 -0400

    some finer changes

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,6 +1,7 @@
 import { ButtonProps } from "./Button.types";
 import { DangerButton, PrimaryButton, SecondaryButton } from "./Button.styles";
-import SettingsIcon from "../../../icons/SettingsIcon";
+import { Text } from "tamagui";
+import DropShadow from "react-native-drop-shadow";
 
 export const Button = ({ btnVariant, title, ...rest }: ButtonProps) => {
   switch (btnVariant) {
@@ -10,9 +11,30 @@ export const Button = ({ btnVariant, title, ...rest }: ButtonProps) => {
       return <SecondaryButton {...rest}>{title}</SecondaryButton>;
     case "danger":
       return (
-        <DangerButton iconAfter={<SettingsIcon />} {...rest}>
-          {title}
-        </DangerButton>
+        <DropShadow
+          style={{
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 0,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 8,
+          }}
+        >
+          <DangerButton {...rest}>
+            <Text
+              style={{
+                fontFamily: "Roboto_700Bold",
+                color: "#fff",
+                fontSize: 16,
+                opacity: 1,
+              }}
+            >
+              {title}
+            </Text>
+          </DangerButton>
+        </DropShadow>
       );
     default:
       return <PrimaryButton {...rest}>{title}</PrimaryButton>;
