90c6e73449b0f623255b5378801179e68a1be21d
Paul Campbell <PaCampbell@mgmresorts.com>
2024-07-03 21:54:47 -0400

    add fonts ill need

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Link, Stack } from "expo-router";
-import React from "react";
+import React, { useEffect } from "react";
 import { Text } from "react-native";
 import { isAndroid, isIOS, isWeb } from "../src/utils";
 import { SessionProvider } from "../src/utils/context";
@@ -7,17 +7,74 @@ import { ApolloProvider } from "@apollo/client";
 import { client } from "../src/utils/api/apollo/apolloClient";
 import { TamaguiProvider } from "tamagui";
 import tamaguiConfig from "../tamagui.config";
-import { useFonts } from "expo-font";
+// import { useFonts } from 'expo-font';
+// import * as Font from "expo-font";
+import 'expo-dev-client';
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from '@expo-google-fonts/roboto';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
-    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  const [loaded, fontsErr] = useFonts({
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
   });
 
+  useEffect(() => {
+    if (loaded || fontsErr) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, fontsErr]);
+
+  if (!loaded && !fontsErr) {
+    return null;
+  }
+
+
+  console.log(loaded);
+  console.log(fontsErr);
+  
   // if (!loaded){
   //   return null;
   // }
+  // useEffect(() => {
+  //   const getFonts = async () => {
+  //     try {
+  //       await Font.loadAsync({
+  //         'RobotoRegular': {uri: '../assets/fonts/Roboto/RobotoRegular.ttf'},
+  //       });
+
+  //     }catch (err){
+  //       console.log(err);
+  //     }
+  //   };
+  //   getFonts();
+  // }, []);
   return (
     <TamaguiProvider config={tamaguiConfig}>
       <SessionProvider>
@@ -37,7 +94,7 @@ export default function RootLayout() {
                 options={{
                   headerLeft: () => (
                     <Link href="/signin">
-                      <Text>Back to login</Text>
+                      <Text style={{fontFamily: 'RobotoRegular'}}>Back to login</Text>
                     </Link>
                   ),
                 }}
@@ -50,3 +107,4 @@ export default function RootLayout() {
     </TamaguiProvider>
   );
 }
+
