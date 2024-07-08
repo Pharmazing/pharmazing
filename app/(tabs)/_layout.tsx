c4db1f0f944f8ff1da946588fc49f353b1bd3e4d
Paul Campbell <PaCampbell@mgmresorts.com>
2024-07-08 15:04:38 -0400

    add Icon architecture & some more stuff to settings box

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,10 +3,7 @@ import { Tabs } from "expo-router";
 import { Drawer } from "expo-router/drawer";
 import { isAndroid, isIOS, isWeb } from "../../src/utils";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import HomeIcon from "../../src/icons/HomeIcon";
-import ActivityIcon from "../../src/icons/ActivityIcon";
-import CartIcon from "../../src/icons/CartIcon";
-import SettingsIcon from "../../src/icons/SettingsIcon";
+import { Icon } from "../../src/components/atoms";
 
 export default function Layout() {
   return (
@@ -17,28 +14,32 @@ export default function Layout() {
             name="home"
             options={{
               title: "Home",
-              tabBarIcon: ({ color }) => <HomeIcon color={color} />,
+              tabBarIcon: ({ color }) => <Icon name="HomeIcon" color={color} />,
             }}
           />
           <Tabs.Screen
             name="activity"
             options={{
               title: "Activity",
-              tabBarIcon: ({ color }) => <ActivityIcon color={color} />,
+              tabBarIcon: ({ color }) => (
+                <Icon name="ActivityIcon" color={color} />
+              ),
             }}
           />
           <Tabs.Screen
             name="cart"
             options={{
               title: "Cart",
-              tabBarIcon: ({ color }) => <CartIcon color={color} />,
+              tabBarIcon: ({ color }) => <Icon name="CartIcon" color={color} />,
             }}
           />
           <Tabs.Screen
             name="settings"
             options={{
               title: "Settings",
-              tabBarIcon: ({ color }) => <SettingsIcon color={color} />,
+              tabBarIcon: ({ color }) => (
+                <Icon name="SettingsIcon" color={color} />
+              ),
             }}
           />
           <Tabs.Screen name="somePage" options={{ tabBarButton: () => null }} />
