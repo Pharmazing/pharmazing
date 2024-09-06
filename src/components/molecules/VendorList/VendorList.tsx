0b810b1c982804371482236a910c3e86536b5b4a
Paul Campbell <PaCampbell@mgmresorts.com>
2024-09-06 19:47:05 -0400

    update home page 2.0

--- src/components/molecules/VendorList/VendorList.tsx
+++ src/components/molecules/VendorList/VendorList.tsx
@@ -1,7 +1,7 @@
 import { VendorListProps } from './VendorList.types';
 import { vendorListStyles } from './VendorList.styles';
 import { useStyles } from 'react-native-unistyles';
-import { Box } from '../../atoms';
+import { Box, Typography } from '../../atoms';
 import { router } from 'expo-router';
 // import { TouchableHighlight } from 'react-native';
 import { VendorCard } from './VendorCard';
@@ -12,15 +12,17 @@ export const VendorList = ({ vendors }: VendorListProps) => {
     const handleVendorClick = (vendorId: string) => {
       router.push(`/pharmacy/${vendorId}`);
     };
-    return vendors?.map((vendor) => {
-      return (
-        <VendorCard
-          key={vendor.vendorId}
-          vendor={vendor}
-          onPress={handleVendorClick}
-        />
-      );
-    });
+    return (
+      vendors?.map((vendor) => {
+        return (
+          <VendorCard
+            key={vendor.vendorId}
+            vendor={vendor}
+            onPress={handleVendorClick}
+          />
+        );
+      }) || <Typography>No vendors found</Typography>
+    );
   };
 
   return <Box>{renderVendorList()}</Box>;
