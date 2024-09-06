735852c2ace7f52e898bd86c67b1bdb2b293d294
Paul Campbell <PaCampbell@mgmresorts.com>
2024-09-06 01:32:38 -0400

    cleanup

--- src/components/molecules/PlacesAutocomplete/PlacesAutocomplete.tsx
+++ src/components/molecules/PlacesAutocomplete/PlacesAutocomplete.tsx
@@ -34,7 +34,6 @@ export const PlacesAutocomplete = React.forwardRef<
     _data: GooglePlaceData,
     details: GooglePlaceDetail | null
   ) => {
-    // console.log('details', details);
     const addressObj: AddressType = {};
     details?.address_components.forEach((item) => {
       const types = item.types;
@@ -91,80 +90,3 @@ export const PlacesAutocomplete = React.forwardRef<
     </Box>
   );
 });
-
-// export const PlacesAutocomplete = ({
-//   onSelect,
-//   placeholder = 'Add new address',
-//   disabled = false,
-//   style,
-// }: PlacesAutocompleteProps) => {
-//   const { styles } = useStyles(placesAutocompleteStyles);
-//   const ref = useRef<GooglePlacesAutocompleteRef>(null);
-
-//   const handleOutsidePress = () => {
-//     if (ref.current?.isFocused()) {
-//       ref.current?.blur();
-//     }
-//   };
-
-//   const handleAutocompleteSelect = (
-//     _data: GooglePlaceData,
-//     details: GooglePlaceDetail | null
-//   ) => {
-//     console.log('details', details);
-//     const addressObj: AddressType = {};
-//     details?.address_components.forEach((item) => {
-//       const types = item.types;
-//       if (types.includes('street_number')) {
-//         addressObj.addressLine1 = item.long_name;
-//       } else if (types.includes('route')) {
-//         addressObj.addressLine1 =
-//           (addressObj.addressLine1 ? addressObj.addressLine1 + ' ' : '') +
-//           item.long_name;
-//       } else if (types.includes('locality')) {
-//         addressObj.city = item.long_name;
-//       } else if (types.includes('administrative_area_level_1')) {
-//         let parish: string | string[] | undefined = item.long_name.split(' ');
-//         parish.pop();
-//         parish = parish.join(' ');
-//         addressObj.parish = parish;
-//         addressObj.zip = parish;
-//       } else if (types.includes('postal_code')) {
-//         addressObj.zip = item.long_name;
-//       } else if (types.includes('country')) {
-//         addressObj.country = item.long_name;
-//       }
-//     });
-//     onSelect?.({
-//       ...addressObj,
-//       latitude: details?.geometry?.location.lat,
-//       longitude: details?.geometry?.location.lng,
-//     });
-//   };
-//   return (
-//     <Box style={[styles.container, style]}>
-//       <OutsidePressHandler onOutsidePress={handleOutsidePress}>
-//         <GooglePlacesAutocomplete
-//           ref={ref}
-//           fetchDetails
-//           styles={{
-//             listView: styles.listView,
-//             textInput: styles.textInput({ disabled }),
-//           }}
-//           enablePoweredByContainer={false}
-//           placeholder={placeholder}
-//           onPress={handleAutocompleteSelect}
-//           minLength={3}
-//           query={{
-//             key: process.env.EXPO_PUBLIC_GOOGLE_PLACES_API_KEY,
-//             language: 'en',
-//             components: 'country:jm',
-//             types: ['address'],
-//           }}
-//           currentLocation
-//           currentLocationLabel="Use current location"
-//         />
-//       </OutsidePressHandler>
-//     </Box>
-//   );
-// };
