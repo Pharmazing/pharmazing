2869668bb63bc1210789ec4e44c562f24555e609
Paul Campbell <vinceterp@outlook.com>
2024-10-16 23:00:23 -0400

    add image to pdp

--- src/components/organisms/ProductDetailPage/ProductDetailPage.tsx
+++ src/components/organisms/ProductDetailPage/ProductDetailPage.tsx
@@ -1,6 +1,32 @@
-import { Typography } from '../../atoms';
+import { useStyles } from 'react-native-unistyles';
+import { Box, Icon, Typography } from '../../atoms';
 import { ProductDetailPageProps } from './ProductDetailPage.types';
-
+import { productDetailPageStyles } from './ProductDetailPage.styles';
+import { router } from 'expo-router';
+import { Image } from 'react-native';
 export const ProductDetailPage = ({ productId }: ProductDetailPageProps) => {
-  return <Typography>{`I'm the pdp ${productId}`}</Typography>;
+  const { styles } = useStyles(productDetailPageStyles);
+  return (
+    <Box style={styles.container}>
+      <Box style={styles.headerContainer}>
+        <Icon
+          onPress={() => router.back()}
+          name="ChevronRightIcon"
+          height={48}
+          width={48}
+          transform={'rotate(180)'}
+        />
+      </Box>
+      <Image
+        source={{ uri: 'https://picsum.photos/900/900' }}
+        style={styles.imageContainer}
+      ></Image>
+      <Box style={styles.bottomSheet}>
+        <Typography size="xxl" weight="500">
+          ProductName
+        </Typography>
+        <Typography size="md">{productId}</Typography>
+      </Box>
+    </Box>
+  );
 };
